59e092a90667dc5135f2692f01ee9501a319a010
NatpacanSribanhad <natpacan.s@kkumail.com>
2023-03-31 10:17:48 +0700

    เกิบได้ละ

--- Login.js
+++ Login.js
@@ -8,48 +8,64 @@ import { getAuth } from 'firebase/auth';
 import { Provider as PaperProvider, Card, List, Button } from 'react-native-paper';
 import Constants from 'expo-constants';
 import PhoneScreen from './Phone';
+// import { withNavigation } from 'react-navigation';
+import { useNavigation } from '@react-navigation/native';
 
+export default function LoginScreen() {
 
-export default function LoginScreen(){
+    const navigation = useNavigation();
 
-
-
-    return(
-        <View style={styles.container}>
-            <View style={{flex:1}} ></View>
-            <View style={{marginTop:40,flex:1}}>
-                <Image
-                source={require('./assets/cloud2.png')}
-                style={{  width: 130,
-                    resizeMode: "contain",
-                    alignSelf: "center", }}
-            />
-            <Image
-                source={require('./assets/aerio.png')}
-                style={{  width: 150,
-                height: 50,
-                resizeMode: "contain",
-                alignSelf: "center",  
-                }}
-            />
-            </View>
-            
-            <View style={{flex:1}}></View>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: 60}}>
-            <TouchableOpacity style={{backgroundColor:"#02466E",padding:15,borderRadius:40}} 
-                onPress={() => navigation.navigate('Phone') }>
-                <Icon name="phone" size={30} color="#ffff" />
-            </TouchableOpacity>
-            <TouchableOpacity style={{backgroundColor:"#02466E",padding:15,borderRadius:40,marginStart:30}} 
-                onPress={() => console.log("Phone button pressed!")}>
-                <Icon name="google" size={30} color="#ffff" />
-            </TouchableOpacity>
-            </View>
+    return (
+      <View style={styles.container}>
+        <View style={{ flex: 1 }}></View>
+        <View style={{ marginTop: 40, flex: 1 }}>
+          <Image
+            source={require('./assets/cloud2.png')}
+            style={{
+              width: 130,
+              resizeMode: 'contain',
+              alignSelf: 'center',
+            }}
+          />
+          <Image
+            source={require('./assets/aerio.png')}
+            style={{
+              width: 150,
+              height: 50,
+              resizeMode: 'contain',
+              alignSelf: 'center',
+            }}
+          />
         </View>
+  
+        <View style={{ flex: 1 }}></View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginBottom: 60,
+          }}>
+          <TouchableOpacity
+            style={{ backgroundColor: '#02466E', padding: 15, borderRadius: 40 }}
+            onPress={() => navigation.navigate('Phone')}>
+            <Icon name="phone" size={30} color="#ffff" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#02466E',
+              padding: 15,
+              borderRadius: 40,
+              marginStart: 30,
+            }}
+            onPress={() => console.log('Google button pressed!')}>
+            <Icon name="google" size={30} color="#ffff" />
+          </TouchableOpacity>
+        </View>
+      </View>
     );
-}
-
-const styles = StyleSheet.create({
+  }
+  
+  const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#023F62',
